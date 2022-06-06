6ff8eb5c5246fb9dad48a7ed08ae736f85138b09
unknown <gaffarihanan@yahoo.com>
2022-06-06 11:57:18 +0700

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> eahayu
+          Edit <code>src/App.js</code> seas
         </p>
         <a
           className="App-link"
